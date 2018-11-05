46936a9ffdf181d9abb2bc1b43f2d879a8ee6f84
Jasna Nilsson <jasna.nilsson@gmail.com>
2018-11-05 04:20:08 +0100

    school

---
+++ ConvertDecimalToBinary/ConvertDecimalToBinary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConvertDecimalToBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            string output;
+
+            Console.Write("Ange ett nummer: ");
+            input = Console.ReadLine();
+
+            int nr = Convert.ToInt32(input);
+            output = "";
+
+            while (nr > 1)
+            {
+                int rmd = nr % 2;
+                output = Convert.ToString(rmd) + output;
+                nr /= 2;
+            }
+
+            output = Convert.ToString(nr) + output;
+            Console.WriteLine(" Resultat i Binary: {0}", output);
+            Console.ReadKey();
+        }
+    }
+}
